bbfdc1d462ec4b4a1b8b8e4a1582d48f651bb53a
Alice <shoh@go.pasadena.edu>
2018-11-28 19:20:47 -0800

    added products

--- js/javascript.js
+++ js/javascript.js
@@ -23,6 +23,11 @@ $(document).ready(function(){
             $('#blue-nav').removeClass('blue-nav');
         }
     });
+    
+    $(".nav .nav-link").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).addClass("active");
+});
 
 }); //document end
 
