8960d68470b3cfc14d793271126ab6b208e9f40c
serelath <shoh@go.pasadena.edu>
2018-12-12 18:36:46 -0800

    fixed footer and changed social to nav

--- js/javascript.js
+++ js/javascript.js
@@ -8,21 +8,21 @@ $(document).ready(function(){
     var distance = $('div').offset().top,
     $window = $(window);
 
-    $window.scroll(function() {
-    if ( $window.scrollTop() >= 92 && $window.width() > 768) {
-            $('header').addClass('logo-scroll');
-            $('#navbar').addClass('position-fixed');
-            $('#logo-sm img').addClass('logo-sm-scroll');
-            $('nav').addClass('nav-scroll');
-            $('#blue-nav').addClass('blue-nav');
-        } else {
-            $('header').removeClass('logo-scroll');
-            $('#navbar').removeClass('position-fixed').addClass('position-absolute');
-            $('#logo-sm img').removeClass('logo-sm-scroll');
-            $('nav').removeClass('nav-scroll');
-            $('#blue-nav').removeClass('blue-nav');
-        }
-    });
+//    $window.scroll(function() {
+//    if ( $window.scrollTop() >= 92 && $window.width() > 768) {
+//            $('header').addClass('logo-scroll');
+//            $('#navbar').addClass('position-fixed');
+//            $('#logo-sm img').addClass('logo-sm-scroll');
+//            $('nav').addClass('nav-scroll');
+//            $('#blue-nav').addClass('blue-nav');
+//        } else {
+//            $('header').removeClass('logo-scroll');
+//            $('#navbar').removeClass('position-fixed').addClass('position-absolute');
+//            $('#logo-sm img').removeClass('logo-sm-scroll');
+//            $('nav').removeClass('nav-scroll');
+//            $('#blue-nav').removeClass('blue-nav');
+//        }
+//    });
     
     $(".nav .nav-link").on("click", function(){
    $(".nav").find(".active").removeClass("active");
